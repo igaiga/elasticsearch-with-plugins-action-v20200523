df50185a8c6b958db69c02d6eec734faac67023e
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-23 10:16:18 +0900

    replace public es actions

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -2,7 +2,6 @@
 
 set -euxo pipefail
 
-# 必須にした引数が存在するかチェック
 if [[ -z $STACK_VERSION ]]; then
   echo -e "\033[31;1mERROR:\033[0m Required environment variable [STACK_VERSION] not set\033[0m"
   exit 1
@@ -10,44 +9,13 @@ fi
 
 docker network create elastic
 
-PLUGINS_STR=`echo ${PLUGINS} | sed -e 's/\n/ /g'` # 引数に含まれている改行文字を置換
-MAJOR_VERSION=`echo ${STACK_VERSION} | cut -c 1`  # メジャーバージョンを取得
-
-PLUGIN_INSTALL_CMD=""
-
-# pluginsを複数インストールするケースがあるのでfor文でコマンドを組み立てる
-if [ "x${PLUGINS_STR}" != "x" ]; then
-    ARRAY=(${PLUGINS_STR})
-    for i in "${ARRAY[@]}"
-    do
-        PLUGIN_INSTALL_CMD+="elasticsearch-plugin install --batch ${i} && "
-    done
-fi
-
-# single node only
-if [ "x${MAJOR_VERSION}" == 'x6' ]; then # メジャーバージョンによって起動オプションを分岐する．
+NODES=${NODES-1}
+for (( node=1; node<=$NODES; node++ ))
+do
+  port=$((9200 + $node - 1))
   docker run \
     --rm \
-    --env "node.name=es1" \
-    --env "cluster.name=docker-elasticsearch" \
-    --env "cluster.routing.allocation.disk.threshold_enabled=false" \
-    --env "bootstrap.memory_lock=true" \
-    --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-    --env "xpack.security.enabled=false" \
-    --env "xpack.license.self_generated.type=basic" \
-    --ulimit nofile=65536:65536 \
-    --ulimit memlock=-1:-1 \
-    --publish "9200:9200" \
-    --detach \
-    --network=elastic \
-    --name="es1" \
-    --entrypoint="" \
-    docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
-    /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
-elif [ "x${MAJOR_VERSION}" == 'x7' ]; then
-  docker run \
-    --rm \
-    --env "node.name=es1" \
+    --env "node.name=es${node}" \
     --env "cluster.name=docker-elasticsearch" \
     --env "cluster.initial_master_nodes=es1" \
     --env "discovery.seed_hosts=es1" \
@@ -58,16 +26,13 @@ elif [ "x${MAJOR_VERSION}" == 'x7' ]; then
     --env "xpack.license.self_generated.type=basic" \
     --ulimit nofile=65536:65536 \
     --ulimit memlock=-1:-1 \
-    --publish "9200:9200" \
+    --publish "${port}:9200" \
     --detach \
     --network=elastic \
-    --name="es1" \
-    --entrypoint="" \
-    docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
-    /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
-fi
+    --name="es${node}" \
+    docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
+done
 
-# 上記で起動したコンテナに対して，curlで死活確認するコンテナを立ち上げる
 docker run \
   --network elastic \
   --rm \
